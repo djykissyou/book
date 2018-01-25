28f9baa8b339136ab1b3362ebd40593521777799
zhaogeppt <zhaogeppt@qq.com>
2018-01-25 13:56:48 +0800

    Default

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,8 +12,8 @@ class CXForm(Form):
     shenfenzheng = StringField(u'身份证号', [DataRequired()])
     kahao = StringField(u'银行卡号', [DataRequired()])
     banji = SelectField(u'毕业所在班级', choices=[('14w1', u'14微机一班'), ('14w2', u'14微机二班'), ('14w3', u'14微机三班')])
-    xuexiao = StringField(u'当前就读大学', [DataRequired()])
-    zhuanye = StringField(u'大学所学专业', [DataRequired()])
+    xuexiao = StringField(u'当前就读大学(已工作则填工作单位)', [DataRequired()])
+    zhuanye = StringField(u'大学所学专业（已工作则填岗位）')
     yuke = BooleanField(u'大学预科请在此选项打钩')
     dianhua = StringField(u'联系电话', [DataRequired()])
 
